889c9d719266014413ee94f7469915a26bead501
Xavier Ngansop <ngaxavi@gmail.com>
2023-03-24 11:50:59 +0100

    chore: bump eventstore version und packages upgrade

--- run-dev-env.sh
+++ run-dev-env.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [[ $(uname -m) == 'arm64' ]]; then
-  image="ghcr.io/eventstore/eventstore:21.10.5-alpha-arm64v8"
+  image="ghcr.io/eventstore/eventstore:22.10.1-alpha-arm64v8"
 else
-  image="eventstore/eventstore:21.10.5-buster-slim"
+  image="eventstore/eventstore:22.10.1-buster-slim"
 fi
 
 EVENTSTORE_IMAGE=$image docker-compose up -d
